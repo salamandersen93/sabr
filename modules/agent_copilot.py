956627ecb8f1fc6864dc09bf2bdf7727c16c155e
salamandersen93 <salamandersen93>
2025-10-21 20:48:08 +0000

    endpoint tweaks contd

--- modules/agent_copilot.py
+++ modules/agent_copilot.py
@@ -37,7 +37,7 @@ class ExplainerAgent:
         
         self.client = WorkspaceClient(host=host, token=token)
         #self.endpoint_name = endpoint
-        self.endpoint_name = "databricks/databricks-gpt-oss-20b"
+        self.endpoint_name = f"databricks/{endpoint}"
         # DEBUGGING
         endpoints = self.client.serving_endpoints.list()
         print("Serving endpoints:", endpoints)
